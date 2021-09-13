92e5fa78fb6d5fee936049f7eeadedaf4b4854ac
Rob Callaghan <rob_cal_uk@hotmail.com>
2021-09-13 23:04:30 +0100

    initial page nav logic setup

---
+++ js/nav.js
@@ -0,0 +1,50 @@
+let observerNavOptions = {
+    rootMargin: '0px',
+    threshold: 0.5
+}
+
+const observerNav = new IntersectionObserver(observerNavCallback, observerNavOptions);
+
+function observerNavCallback(entries, observerNav) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            currentId = entry.target.id;
+            updateNav(currentId)
+        }
+    });
+};
+
+let targetNav = '.chart-section';
+document.querySelectorAll(target).forEach((i) => {
+    if (i) {
+        observerNav.observe(i);
+    }
+});
+
+const nav = document.getElementById('nav');
+const navLinks = document.querySelectorAll('[data-target]');
+
+function updateNav(id) {
+    let navTarget = document.querySelector(`[data-target=${id}`)
+
+    // add/remove classes to display active/inactive + toggle data-active value
+    for (const link of navLinks) {
+        if (link.getAttribute('data-active') === 'false') {
+
+            if (link === navTarget) {
+                link.classList.remove("opacity-50")
+                link.classList.add("font-semibold")
+                navTarget.setAttribute('data-active', 'true');
+            }
+        } else {
+
+            if (link !== navTarget) {
+                link.classList.add("opacity-50")
+                link.classList.remove("font-semibold")
+                link.setAttribute('data-active', 'false');
+            }
+        }
+    }
+    navTarget.setAttribute('data-active', 'true');
+
+}
