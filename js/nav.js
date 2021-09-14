97c924f67e58429ef0f25405feb9b44c78545aff
Rob Callaghan <rob_cal_uk@hotmail.com>
2021-09-14 07:39:30 +0100

    changing page hash based on current section

--- js/nav.js
+++ js/nav.js
@@ -9,13 +9,15 @@ function observerNavCallback(entries, observerNav) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             currentId = entry.target.id;
-            updateNav(currentId)
+            updateNav(currentId).then(function () {
+                // history.pushState(null, null, `#${currentId}`);
+            })
         }
     });
 };
 
 let targetNav = '.chart-section';
-document.querySelectorAll(target).forEach((i) => {
+document.querySelectorAll(targetNav).forEach((i) => {
     if (i) {
         observerNav.observe(i);
     }
@@ -28,23 +30,22 @@ function updateNav(id) {
     let navTarget = document.querySelector(`[data-target=${id}`)
 
     // add/remove classes to display active/inactive + toggle data-active value
+    // return new Promise(function (resolve, reject) {
     for (const link of navLinks) {
-        if (link.getAttribute('data-active') === 'false') {
-
-            if (link === navTarget) {
-                link.classList.remove("opacity-50")
-                link.classList.add("font-semibold")
-                navTarget.setAttribute('data-active', 'true');
-            }
-        } else {
-
-            if (link !== navTarget) {
-                link.classList.add("opacity-50")
-                link.classList.remove("font-semibold")
-                link.setAttribute('data-active', 'false');
-            }
+        if (link.getAttribute('data-active') === 'false' && link === navTarget) {
+            link.classList.remove("opacity-50")
+            link.classList.add("font-semibold")
+            navTarget.setAttribute('data-active', 'true');
+        } else if (link !== navTarget) {
+            link.classList.add("opacity-50")
+            link.classList.remove("font-semibold")
+            link.setAttribute('data-active', 'false');
         }
     }
     navTarget.setAttribute('data-active', 'true');
+    history.replaceState(null, null, `#${currentId}`);
+
+    // resolve()
+    // })
 
 }
